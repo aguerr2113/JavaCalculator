f86ed300d3d8d318b8443716924f0ce7566a5cd3
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-03-15 20:46:56 -0500

    Created a basic calculator.

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,15 +2,42 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        Scanner bucky = new Scanner(System.in);
-        double fnum,snum,answer;
-        System.out.println("Enter first Number: ");
-        fnum = bucky.nextDouble();
-        
-        System.out.println("Enter Second Number: ");
-        snum = bucky.nextDouble();
-        answer = fnum + snum;
+        int operator, n1, n2;
+        System.out.println("1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide");
 
-        System.out.println("The sum is " + answer);
+        System.out.println("Choose Operator: ");
+        Scanner sc = new Scanner(System.in);
+        operator = sc.nextInt();
+
+        System.out.print("Enter first number : ");
+        n1 = sc.nextInt();
+
+        System.out.print("Enter Second number : ");
+        n2 = sc.nextInt();
+
+        int result = 0;
+
+        switch (operator) {
+        case 1:
+            result = n1 + n2;
+            break;
+            
+        case 2:
+            result = n1 - n2;
+            break;
+            
+        case 3:
+            result = n1 * n2;
+            break;
+            
+        case 4:
+            result = n1 / n2;
+            break;
+        
+        default:
+            System.out.println("Entered Operator is not valid");
+        }
+        
+        System.out.println("Result is : " + result);
     }
 }
